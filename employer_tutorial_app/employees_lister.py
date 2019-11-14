70ee7f584bba2a49acb26ed0f8c12621f603a5bf
Moudbuom <mahmoudbaibars@gmail.com>
2019-11-14 23:48:23 +0200

    employee_name as string receives input

---
+++ employer_tutorial_app/employees_lister.py
@@ -0,0 +1,10 @@
+
+
+while True:
+  user_input = input("Please Enter Employee's name or 0 to Exit ... ")
+  if user_input == '0':
+    break
+  else:
+    employee_name = user_input
+
+print(employee_name)
