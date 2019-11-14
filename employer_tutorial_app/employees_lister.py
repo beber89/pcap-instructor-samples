5d468c5155c9170fb2323b273b5abd07092ceb6c
Moudbuom <mahmoudbaibars@gmail.com>
2019-11-14 23:50:33 +0200

    Employees receiving input to a list

--- employer_tutorial_app/employees_lister.py
+++ employer_tutorial_app/employees_lister.py
@@ -1,10 +1,11 @@
 
+employees = []
 
 while True:
   user_input = input("Please Enter Employee's name or 0 to Exit ... ")
   if user_input == '0':
     break
   else:
-    employee_name = user_input
+    employees.append(user_input)
 
-print(employee_name)
+print(employees)
