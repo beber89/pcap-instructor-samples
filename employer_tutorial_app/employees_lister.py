3935308202e5d476ca23de27080faec653172d67
Moudbuom <mahmoudbaibars@gmail.com>
2019-11-17 10:03:36 +0200

    Exceptions are added

--- employer_tutorial_app/employees_lister.py
+++ employer_tutorial_app/employees_lister.py
@@ -1,5 +1,5 @@
 # Specs: 
-# Strings:
+# [X] Strings:
 ## [X] Employess, list of dictionaries representing employee
 ## [X] Employee can be entered using comma separated values
 ## Or can be entered one attribute at a time
@@ -10,27 +10,35 @@
 ### Even if implementation is one liner
 ### It becomes easier to understand logic from function name
 ## Port functions into different module
-# Exceptions:
-## ValueError when converting erroneous Employee age to integer
-## KeyboardInterrupt Exception on the while loop
+# [X] Exceptions:
+## [X] ValueError when converting erroneous Employee age to integer
+## [X] KeyboardInterrupt Exception on the while loop
 
 employees = []
 
-while True:
-  user_input = input(\
-    "Please Enter Employee's name and age separated by comma\n"\
-    +"or enter 0 to Exit ...\n")
-  if user_input == '0':
-    break
-  elif ',' not in user_input:
-    print('please enter name and age separated by comma')
-    continue
-  else:
-    name, age = user_input.split(',')
-    if not name.isalpha() or not age.isdigit():
-      print("Please Enter a Valid name and age !!!")
+try:
+  while True:
+    user_input = input(\
+      "Please Enter Employee's name and age separated by comma\n"\
+      +"or enter 0 to Exit ...\n")
+    if user_input == '0':
+      break
+    elif ',' not in user_input:
+      print('please enter name and age separated by comma')
       continue
-    employees.append({'name': name, 'age': age})
+    else:
+      [name, age] = user_input.split(',')
+      if not name.isalpha():
+        print("Please Enter a Valid name !!!")
+        continue
+      try:
+        employees.append({'name': name, 'age': int(age)})
+      except ValueError:
+        print("Please Enter a Valid age !!!\n")
+        continue
+except KeyboardInterrupt:
+  print("User forcibly exited the application")
+  print(employees)
+
       
 
-print(employees)
